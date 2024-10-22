7e7f5be1a5a7f0e12969f075cc62e6479776786b
Erik Hallgren <erikhallgren@live.se>
2024-10-22 12:20:45 +0200

    save works but SelectDeck doesn't refresh correctly yet.

--- src/components/SelectDeck.tsx
+++ src/components/SelectDeck.tsx
@@ -13,9 +13,6 @@ interface SelectDeckProps {
 export const SelectDeck: React.FC<SelectDeckProps> = ({ decks, onLoadButton }) => {
 	// displays a list of saved decks and buttons to load, delete and rename.
 
-	// const [decksArray, setDecksArray] = useState<IDeck[]>(decks);
-	// console.log(decks);
-
 	const [targetDeck, setTargetDeck] = useState<IDeck | null>(null); // contains the deck that is currently selected in the <select> element
 
 	let [cardCounts, setCardCounts] = useState<{ main: number; sideboard: number }>({ main: 0, sideboard: 0 });
@@ -56,27 +53,6 @@ export const SelectDeck: React.FC<SelectDeckProps> = ({ decks, onLoadButton }) =
 		//
 		<>
 			<section className="selectDeck">
-				{/* <div id="taskFormWrapper">
-					<article>
-						<h1>To do list</h1>
-						<p>Like task manager, but for people.</p>
-					</article>
-					<form onSubmit={handleSubmit} className="task-form" id="taskForm">
-						<div className="flex-vertical">
-							<input type="text" name="taskTitle" id="taskTitle" placeholder="Task title" className="textbox" value={title} onChange={(e) => setTitle(e.target.value)} />
-							<textarea name="taskText" id="taskText" rows={12} placeholder="Task text" className="textbox" value={description} onChange={(e) => setDescription(e.target.value)} />
-						</div>
-
-						<div className="flex-horizontal">
-							<input type="submit" value="Submit" id="submitTaskBtn" className="btn" />
-							<div className="input-with-label">
-								<label htmlFor="taskIsDoneChkBox">Task done:</label>
-								<input onChange={(e) => setIsDone(e.target.checked)} type="checkbox" name="taskIsDone" id="taskIsDoneChkBox" className="checkbox" checked={isDone} value="isDone" />
-							</div>
-						</div>
-					</form>
-				</div> */}
-
 				<div className="selectDeckWrapper">
 					<button onClick={handleTestButton} className="btn" id="testBtn" value="test">
 						TEST
@@ -90,10 +66,6 @@ export const SelectDeck: React.FC<SelectDeckProps> = ({ decks, onLoadButton }) =
 					</select>
 
 					<div className="flex-horizontal">
-						{/* <button onClick={handleToggleDone} className="btn" id="toggleDoneBtn" value="toggleDone" title="Mark deck as done / not done.">
-							{"✔️|➖"}
-						</button> */}
-
 						<button onClick={handleLoadButton} className="btn" id="loadDeckBtn" value="Load">
 							Load
 						</button>
@@ -110,7 +82,6 @@ export const SelectDeck: React.FC<SelectDeckProps> = ({ decks, onLoadButton }) =
 						<div>
 							<h2>{targetDeck.name}</h2>
 							<h3>ID: {targetDeck.id}</h3>
-							{/* <h2>Main: {targetDeck.main.length} cards</h2> */}
 							<h2>Main: {cardCounts.main} cards</h2>
 							{targetDeck.main.map((card, index) => (
 								<p key={index}>
@@ -123,18 +94,33 @@ export const SelectDeck: React.FC<SelectDeckProps> = ({ decks, onLoadButton }) =
 									) : (
 										"✔"
 									)}
-									{card.count} <a>{card.name}</a>
+									{card.count}{" "}
+									<a target="_blank" href={card.is_real ? card.card_info?.scryfall_uri : "#"}>
+										{card.name}
+									</a>
 								</p>
 							))}
 							<h2>Sideboard: {cardCounts.sideboard} cards</h2>
-							{/* <h3>Sideboard: {targetDeck.sideboard.length} cards</h3> */}
 							{targetDeck.sideboard.map((card, index) => (
+								// <p key={index}>
+								// 	{card.count} {card.name}
+								// </p>
 								<p key={index}>
-									{card.count} {card.name}
+									{!card.is_real ? (
+										<span>
+											<a className="decklist-entry-warning" title="⚠Error: invalid/unverified card name. If spelling is correct, try saving this deck again to retrigger verification." target="_blank">
+												{<span className="warning-emoji">⚠</span>}
+											</a>
+										</span>
+									) : (
+										"✔"
+									)}
+									{card.count}{" "}
+									<a target="_blank" href={card.is_real ? card.card_info?.scryfall_uri : "#"}>
+										{card.name}
+									</a>
 								</p>
 							))}
-
-							{/* Add more details as needed */}
 						</div>
 					) : (
 						<p>Select a deck to display details here.</p>
